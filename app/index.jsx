1de157672cd67615df1a70e6cc9adefc9940c3fd
ZaeemShakir <shakirzaeem@gmail.com>
2024-07-19 21:41:05 +0100

    5th commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import {  Text, View } from 'react-native';
 
+import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Evento</Text>
+      <Text className="text-3xl font-serif">Evento</Text>
+      <Link className='text-xl text-blue-500' href={'/profile'}>Go To Profile</Link>
      
     </View>
   );
