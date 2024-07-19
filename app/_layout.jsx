190da4fcc557b699958d4918441d05900a2143ab
ZaeemShakir <shakirzaeem@gmail.com>
2024-07-19 21:03:11 +0100

    4th commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Slot} from 'expo-router'
 
 const _layout = () => {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <Slot/>
   )
 }
 
 export default _layout
 
-const styles = StyleSheet.create({})
